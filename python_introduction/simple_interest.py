acbe93b3917bcad987c2e1d60af19a560d7c2360
Yonatan <desisayonatan77@gmail.com>
2024-06-30 00:00:28 -0700

    Python_simple_project

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Python script that calculates the simple interest earned on an investment over a period of time.
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+print(f"The simple interest is {interest}")
