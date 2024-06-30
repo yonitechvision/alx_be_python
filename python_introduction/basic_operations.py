0fda600461dcc82f01a42c4c86e92a7047a0b6f6
Yonatan <desisayonatan77@gmail.com>
2024-06-29 23:55:45 -0700

    Python_operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Write a Python script that performs basic arithmetic operations with two predefined numbers.
+number1 = 10
+number2 = 5
+sum = number1 + number2
+sub = number1 - number2
+mul = number1 * number2
+print(f"Addition of 10 and 5 is {sum}")
+print(f"Subtraction of 10 and 5 is {sub}")
+print(f"Multiplication of 10 and 5 is {mul}")
+
