2dbcdb558d5ee4dfccb3d58faeb4a99f8277e3c0
Yonatan <desisayonatan77@gmail.com>
2024-07-12 07:40:03 -0700

    Rec_area

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,5 +1,4 @@
 # write a Python script that calculates the area of a rectangle.
 length = 10
 width = 5
-Area = length * width
-print(f"The area of the rectangles is : {Area}")
+print(f"The area of the rectangles is : {Area = length * width}")
