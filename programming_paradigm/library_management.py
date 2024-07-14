086b6ea6b1da49737957f05738f94b1c0d4d1d45
Yonatan <desisayonatan77@gmail.com>
2024-07-14 08:44:43 -0700

    library

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,57 @@
+class Book:
+    def init(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        return not self._is_checked_out
+
+class Library:
+    def init(self):
+        self._books = []
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if book.check_out():
+                    print(f"Checked out: {title}")
+                    return
+                else:
+                    print(f"The book '{title}' is already checked out.")
+                    return
+        print(f"The book '{title}' is not in the library.")
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if book.return_book():
+                    print(f"Returned: {title}")
+                    return
+                else:
+                    print(f"The book '{title}' was not checked out.")
+                    return
+        print(f"The book '{title}' is not in the library.")
+
+    def list_available_books(self):
+        available_books = [book for book in self._books if book.is_available()]
+        if available_books:
+            for book in available_books:
+                print(f"{book.title} by {book.author}")
+        else:
+            print("No available books at the moment.")
