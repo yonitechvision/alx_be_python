77cd652c07f3f27bb594ef07ffa781da2eb66cbb
Yonatan <desisayonatan77@gmail.com>
2024-07-13 20:43:56 -0700

    bankAccount

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -18,7 +18,7 @@ import sys
 from bank_account import BankAccount
 
 def main():
-    account = BankAccount()  # Example starting balance
+    account = BankAccount(100)  # Example starting balance
     if len(sys.argv) < 2:
         print("Usage: python main.py <command>:<amount>")
         print("Commands: deposit, withdraw, display")
